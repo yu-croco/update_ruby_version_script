c88c1d20f246a613b236e6dde10df3b488158b76
yu-croco <yuki.kita22@gmail.com>
2019-07-14 20:49:26 +0900

    add simple ruby version update script for remote environment

---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Install specified Ruby version on your EC2 via rbenv and change your global Ruby version.
+CMDNAME=`basename $0`
+
+function usage {
+  cat <<EOM
+Usage: $(basename "$0") [OPTION]...
+
+  -h VALUE  server host name
+  -v VALUE  ruby version
+EOM
+  exit 2
+}
+
+while getopts h:v: OPT
+do
+  case $OPT in
+    "h" ) FLG_A="TRUE" ; HOST="$OPTARG" ;;
+    "v" ) FLG_B="TRUE" ; RUBY_VERSION="$OPTARG" ;;
+  esac
+done
+
+if [ "$FLG_A" = "TRUE" ] && [ "$FLG_B" = "TRUE" ]; then
+  echo "Start Installing Ruby ${RUBY_VERSION} into ${HOST}..."
+  ssh $HOST "sudo su --login -c 'cd /opt/rbenv/plugins/ruby_build && git pull origin master && ./install.sh && rbenv install ${RUBY_VERSION} && rbenv global ${RUBY_VERSION} && rbenv exec gem install bundler && rbenv rehash' root"
+else
+  usage
+fi
